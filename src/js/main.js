0c15a99cf15d7a02e9d567665a6243525860a74e
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-10-30 21:45:33 +0800

    Menambahkan konten pada category content

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,28 @@ import latestPostContent from './script/latest-post-content';
 import popularPostContent from './script/popular-post-content';
 import categoryContent from './script/category-content';
 
-const baseUrl = 'https://newsapi.org/v2/everything?';
-featureContent(baseUrl);
-latestPostContent(baseUrl, '4');
-popularPostContent(baseUrl, '5');
-
 const main = () => {
+	const baseUrl = 'https://newsapi.org/v2/everything?';
+	featureContent(baseUrl);
+	latestPostContent(baseUrl, '4');
+	popularPostContent(baseUrl, '5');
+
+	// Get Needed Elements
 	const navLiElements = document.querySelectorAll('nav ul li');
 	const navAElements = document.querySelectorAll('nav ul li a');
 	const header = document.querySelector('header');
 	const featuredForMembers = document.querySelector('featured-for-members');
 	const latestPost = document.querySelector('latest-posts');
 	const popularPosts = document.querySelector('popular-posts');
-	const categoryContentContainer = document.querySelector('category-content');
+	const categoryContentContainer = document.querySelector('.category-content');
 
+	// Check if the page is refreshed
 	if (localStorage.getItem('navLinkID') != 'home') {
 		categoryContent(baseUrl, localStorage.getItem('navLinkID'));
+	} else {
+		categoryContentContainer.classList.add('hidden');
+		categoryContentContainer.classList.remove('flex');
+		categoryContentContainer.classList.remove('mt-20');
 	}
 
 	navLiElements.forEach(function (navLiElement) {
@@ -34,6 +40,7 @@ const main = () => {
 			popularPosts.classList.remove('hidden');
 		}
 
+		// Nav Menu Underline Active Behavior
 		if (localStorage.getItem('navLinkID') == navLiElement.id) {
 			navAElements.forEach(function (navAElement) {
 				if (navAElement.classList.contains('underline')) {
@@ -60,6 +67,11 @@ const main = () => {
 				popularPosts.classList.add('hidden');
 				localStorage.setItem('navLinkID', this.id);
 				localStorage.setItem('hidden-home-content', true);
+				categoryContentContainer.innerHTML = '';
+				categoryContent(baseUrl, localStorage.getItem('navLinkID'));
+				categoryContentContainer.classList.remove('hidden');
+				categoryContentContainer.classList.add('flex');
+				categoryContentContainer.classList.add('mt-20');
 			} else {
 				header.lastElementChild.classList.remove('hidden');
 				featuredForMembers.classList.remove('hidden');
@@ -67,6 +79,10 @@ const main = () => {
 				popularPosts.classList.remove('hidden');
 				localStorage.setItem('navLinkID', this.id);
 				localStorage.setItem('hidden-home-content', false);
+				categoryContentContainer.innerHTML = '';
+				categoryContentContainer.classList.add('hidden');
+				categoryContentContainer.classList.remove('flex');
+				categoryContentContainer.classList.remove('mt-20');
 			}
 		});
 	});
--- src/js/script/category-content.js
+++ src/js/script/category-content.js
@@ -13,28 +13,24 @@ const categoryContent = async (baseUrl, key) => {
 
 	const categoryContentContainer = document.querySelector('.category-content');
 	const news = await getNewsBasedOnCategory(10);
-	console.log(news);
 
 	news.forEach((data) => {
 		categoryContentContainer.innerHTML += `
-       <div class="card w-96 bg-white shadow-xl">
-        <figure>
-          <img src="${data.urlToImage}" alt="${key}" class="w-full h-48 object-cover"/>
-        </figure>
-        <div class="card-body">
-          <a href="${data.url}" class="card-title hover:underline">
-            ${data.title}
-          </a>
-          <p class="h-12 line-clamp-2 text-ellipsis mb-4 mt-2">
-            ${data.description}
-          </p>
-          <div class="card-actions justify-end">
-            <div class="badge badge-inline">${data.source.name}</div>
-            <div class="badge badge-outline">${formatDate(data.publishedAt)}</div>
-          </div>
-        </div>
-      </div>
-  `;
+	     <div class="card w-96 bg-white shadow-xl">
+	      <figure>
+	        <img src="${data.urlToImage}" alt="${key}" class="w-full h-48 object-cover"/>
+	      </figure>
+	      <div class="card-body flex flex-col justify-between">
+	        <a href="${data.url}" class="card-title hover:underline">
+	          ${data.title}
+	        </a>
+	        <div class="card-actions justify-end mt-5">
+	          <div class="badge badge-inline">${data.source.name}</div>
+	          <div class="badge badge-outline">${formatDate(data.publishedAt)}</div>
+	        </div>
+	      </div>
+	    </div>
+	`;
 	});
 };
 
